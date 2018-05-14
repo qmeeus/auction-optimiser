e59edf42dea39e67c4fd2e89678c58c2eff3f7f3
qmeeus <qmeeus@gmail.com>
2018-05-14 21:09:30 +0200

    reoptimise

--- visualisation.py
+++ visualisation.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
         
-def label_barchart(ax):
-    text_settings = dict(fontsize=9, fontweight='bold', color="Silver")
+def label_barchart(ax, fmt="{:.1%}"):
+    text_settings = dict(fontsize=12, fontweight='bold', color="White")
     rects = ax.patches
     for i, rect in enumerate(rects):
         x_pos = rect.get_x() + rect.get_width() / 2
-        label = "{:.1%}".format(rect.get_height())
-        ax.text(x_pos, .05, label, ha='center', va='center', **text_settings)
+        label = fmt.format(rect.get_height())
+        ax.text(x_pos, .5, label, ha='center', va='center', **text_settings)
